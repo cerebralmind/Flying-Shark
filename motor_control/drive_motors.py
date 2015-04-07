2ba53b2875eb1de2dd67c85406b91908a625fc9b
Devin Vitone <vitone@gmail.com>
2015-04-07 05:37:25 +0000

    python 3 implementation of motor control

--- motor_control/drive_motors.py
+++ motor_control/drive_motors.py
@@ -6,17 +6,27 @@ class Shark(object):
     def __init__(self):
         config_fp = open('motor_config.yml')
         self.motor_config = load(config_fp.read())
+        config_fp.close()
         
         # Setup port direction and initial values
-        for (motor_name, motor) in self.motor_config['pins'].iteritems():
-            for (direction_name, direction) in motor.iteritems():
-                for (gpio_name, gpio) in direction.iteritems():
+        for (motor_name, motor) in self.motor_config['pins'].items():
+            for (direction_name, direction) in motor.items():
+                for (gpio_name, gpio) in direction.items():
                     try:
                         gpio['fp'] = open(gpio['path'])
                         gpio['fp'].write(gpio['initialize'])
+                    except Exception as e:
+                        print("failed to write '{}' to '{}': {}".format(gpio['initialize'], gpio['path'], e))
+
+    def __del__(self): 
+        for (motor_name, motor) in self.motor_config['pins'].items():
+            for (direction_name, direction) in motor.items():
+                for (gpio_name, gpio) in direction.items():
+                    try:
+                        gpio['fp'].close() 
                     except:
-                        print "failed to write '%s' to '%s'" % (gpio['initialize'], gpio['path'])
-            
+                        pass
+
     def coordinate_move(self, coord_x, coord_y, coord_z):
         ''' Precise control for use with computer vision'''
         pass
@@ -31,16 +41,16 @@ class Shark(object):
         '''
 
     def move(self, motor, direction, duration=0.1):
-	
-		# TODO :: Add the ability to drive pitch and tail at the same time
-        for (direction_name, direction_pin) in self.motor_config['pins'][motor].iteritems():
+    
+        # TODO :: Add the ability to drive pitch and tail at the same time
+        for (direction_name, direction_pin) in self.motor_config['pins'][motor].items():
             value = 'low'
             if direction_name == direction:
                 value = 'high'
             try:
                 direction_pin['value']['fp'].write(value)
             except:
-                print "failed to write '%s' to '%s'" % (value, direction_pin['value']['path'])           
+                print("failed to write '{}' to '{}'".format(value, direction_pin['value']['path']))
 
 
 
